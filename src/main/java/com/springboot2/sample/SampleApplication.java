650e38aa91c5b3e7e1dfec25339f7387c75dafeb
lngo0 <longngo.gm@gmail.com>
2023-08-10 16:22:41 -0500

    Rest controller and dto

--- src/main/java/com/springboot2/sample/SampleApplication.java
+++ src/main/java/com/springboot2/sample/SampleApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.springboot2.sample;
+package com.springboot2.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/springboot2/sample/controller/SampleController.java
@@ -0,0 +1,50 @@
+package com.springboot2.sample.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class SampleController {
+    @RequestMapping(path = "/defaultHello", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Object> hello(@RequestParam(value = "message", required = false) String message) {
+      HashMap<String, String> map = new HashMap<>();
+      String finalMessage = "Hello ";
+      finalMessage = finalMessage + message;
+      if (message == null) {
+          map.put("echo", "Hello World!");
+      } else {
+          if (!message.isEmpty()) {
+              map.put("echo", finalMessage);
+          } else {
+              map.put("echo", "Hello World!");
+          }
+      }
+        return ResponseEntity.ok(map);
+    }
+
+    @RequestMapping(path = "/customHello", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Object> customHello(@RequestParam(value = "message", required = false) String message) {
+      HashMap<String, String> map = new HashMap<>();
+      String finalMessage = "Custom ";
+      map.put("echo", finalMessage);
+
+        if (message == null) {
+            return ResponseEntity.badRequest().body(map);
+        } else {
+            if (!message.isEmpty()) {
+                finalMessage = finalMessage + message;
+                map.put("echo", finalMessage);
+            }
+            return ResponseEntity.ok(map);
+        }
+
+    }
+}
