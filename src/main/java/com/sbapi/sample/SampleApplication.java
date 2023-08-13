8f9162d452f20c41131db15b68b90d229e1100bb
lngo0 <longngo.gm@gmail.com>
2023-08-13 17:21:20 -0500

    fix sample test class

--- src/main/java/com/sbapi/sample/SampleApplication.java
+++ src/main/java/com/sbapi/sample/SampleApplication.java
@@ -1,11 +1,10 @@
-package com.springboot2.sample;
+package com.sbapi.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
--- src/test/java/com/sbapi/sample/controller/SampleApplicationTests.java
+++ src/test/java/com/sbapi/sample/controller/SampleApplicationTests.java
@@ -1,13 +1,12 @@
-package com.springboot2.sample;
+package com.sbapi.sample.controller;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,17 +16,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 public class SampleApplicationTests {
     @Autowired
     private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-    }
 
     @Test
     public void defaultHelloTest() throws Exception {
